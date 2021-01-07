c66bc19029855ebd4737ea169ea230392d311ecb
Felipe <Felipe.dom.martins@gmail.com>
2021-01-07 13:43:50 -0300

    Categories CRUD with Rest

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -10,6 +10,31 @@ const create = async (req, res) => {
     })
 }
 
+const remove = async (req, res) => {
+    await Category.remove(req.params.id)
+    res.send({
+        success: true
+    })
+}
+
+const put = async (req, res) => {
+    const { category } = req.body
+    await Category.update(req.params.id, [category])
+    res.send({
+        success: true
+    })
+}
+const getAll = async (req, res) => {
+    const categories = await Category.findAll()
+    res.send({
+        categories
+    })
+
+}
+
 module.exports = {
-    create
+    create,
+    remove,
+    put,
+    getAll
 }
--- routes/categories.js
+++ routes/categories.js
@@ -3,5 +3,8 @@ const router = express.Router()
 const categoriesController = require('../controllers/categoriesController')
 
 router.post('/', categoriesController.create)
+router.delete('/:id', categoriesController.remove)
+router.put('/:id', categoriesController.put)
+router.get('/', categoriesController.getAll)
 
 module.exports = router
